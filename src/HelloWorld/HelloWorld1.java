457d5257f177ec43a7141b3df43f44996d9e1937
User <User@DESKTOP-4FTJ4J5>
2021-01-04 23:47:04 +0900

    Hello HanRiver 메세지 추가

--- src/HelloWorld/HelloWorld1.java
+++ src/HelloWorld/HelloWorld1.java
@@ -5,5 +5,6 @@ public class HelloWorld1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hi HanRiver");
+		System.out.println("Hello HanRiver");
 	}
 }
